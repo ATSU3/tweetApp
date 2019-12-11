d21339172e7b002c0e5ad397b459825685857842
ATSU3 <mifutsua@gmail.com>
2019-12-11 21:20:16 +0900

    removeNameColumn

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_11_082358) do
+ActiveRecord::Schema.define(version: 2019_12_11_121401) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
     t.text "text"
     t.text "image"
     t.datetime "created_at"
